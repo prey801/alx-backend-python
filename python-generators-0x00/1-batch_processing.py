f40b974bbb74cae784519b9871d7bf33e8cc26cf
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-12 04:15:12 +0300

    mm

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="alx_user",
+        password="StrongP@ssw0rd!",
+        database="ALX_prodev"
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):         # Loop 1
+        for user in batch:                                    # Loop 2
+            if int(user['age']) > 25:
+                print(user)
