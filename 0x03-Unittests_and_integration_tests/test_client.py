e35c0dac45f993c57e71b1e97c08dc2ce958160a
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-19 02:30:30 +0300

    m

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,13 +8,13 @@ from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
 
-@parameterized_class((
-    "org_payload",
-    "repos_payload",
-    "expected_repos",
-    "apache2_repos",
-), [
-    (org_payload, repos_payload, expected_repos, apache2_repos)
+@parameterized_class([
+    {
+        "org_payload": org_payload,
+        "repos_payload": repos_payload,
+        "expected_repos": expected_repos,
+        "apache2_repos": apache2_repos,
+    }
 ])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integration tests with @parameterized_class"""
@@ -46,6 +46,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             client.public_repos(license="apache-2.0"),
             self.apache2_repos
         )
-        
+
+
 if __name__ == "__main__":
     unittest.main()
