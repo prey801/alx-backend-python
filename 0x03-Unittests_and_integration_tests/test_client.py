f162579000e900e4d5580d191cd6725f21cc7adb
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-19 02:04:28 +0300

    m

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python3
-from unittest import TestCase
-from unittest.mock import patch, Mock
-from parameterized import parameterized
-from client import GithubOrgClient
-import client
+"""GitHub API client."""
+
+from utils import get_json, memoize
 
 
-class TestGithubOrgClient(TestCase):
-    """
-    Unit tests for GithubOrgClient.org method.
-    """
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
-    @patch('client.GithubOrgClient.get_json')
-    def test_org(self, org_name, mock_get_json):
-        """
-        Test that GithubOrgClient.org returns correct data 
-        and get_json  once every call with correct URL.
-        """
-        expected_payload = {"login": org_name}
-        mock_get_json.return_value = expected_payload
+class GithubOrgClient:
+    """Client for GitHub organization operations."""
 
-        client = GithubOrgClient(org_name)
-        result = client.org()
+    def __init__(self, org_name: str):
+        self.org_name = org_name
 
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
-        self.assertEqual(result, expected_payload)
+    @memoize
+    def org(self):
+        """Get organization details."""
+        return get_json(f"https://api.github.com/orgs/{self.org_name}")
+
+    @property
+    def _public_repos_url(self):
+        """Get the URL to the public repositories of the organization."""
+        return self.org.get("repos_url")
