f08a625ddb189ae13ea0ecd0bb05c8e42d6ce2e3
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-20 11:21:50 +0300

    m

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    @action(detail=True, methods=['post'])
+    def send_message(self, request, pk=None):
+        conversation = self.get_object()
+        message_data = request.data
+        message_data['conversation'] = conversation.id
+        
+        serializer = MessageSerializer(data=message_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        conversation_id = self.request.query_params.get('conversation', None)
+        if conversation_id:
+            return Message.objects.filter(conversation_id=conversation_id)
+        return Message.objects.all()
