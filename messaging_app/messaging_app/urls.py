8d5bf4de434e07a35821cfca42ede22faaba7b7f
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-20 11:40:50 +0300

    m

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,8 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  
+    path('api/', include('chats.urls')), 
+    path('api-auth/', include('rest_framework.urls')), 
 ]
