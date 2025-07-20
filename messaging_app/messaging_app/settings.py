488ea7ac1715a46772b8d7a95d3c2496ff95e3a7
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-20 11:03:00 +0300

    m

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -80,7 +80,17 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
